b671e9268a5dbf50892adf72115c449d963b094b
Ignacio Soler <ignaciojsoler@gmail.com>
2023-12-10 20:25:57 -0300

    Feature: data folder

---
+++ src/data/pages.ts
@@ -0,0 +1,26 @@
+export const pages = [
+    {
+        name: "Home",
+        href: "/",
+    },
+    {
+        name: "About",
+        href: "/about",
+    },
+    {
+        name: "Menu",
+        href: "/menu",
+    },
+    {
+        name: "Reservations",
+        href: "/reservations",
+    },
+    {
+        name: "Order Online",
+        href: "/order",
+    },
+    {
+        name: "Login",
+        href: "/login",
+    }
+];
